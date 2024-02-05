ccba40ec2f2ba2eb489aacb00575b6c40a85637c
Mariana Téllez <tellezresendizmariana@gmail.com>
2024-02-05 13:36:45 -0600

    Updated menu

--- src/Menu.js
+++ src/Menu.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Menu.css";
+import MX from "./img/MX.png";
+import US from "./img/US.png";
 
 export default function Menu() {
   return (
     <div className="Menu">
-      <section>
+      <section className="menu-navbar">
         <div>
           <p>February 22, 2024</p>
         </div>
@@ -12,9 +14,27 @@ export default function Menu() {
           <h1>Today News</h1>
         </div>
         <div>
-          <p>US</p>
-          <p>MX</p>
+          <a>
+            <img src={MX} alt="MX" id="icon-flag" />
+            MX
+          </a>
+          <a>
+            <img src={US} alt="US" id="icon-flag" />
+            US
+          </a>
         </div>
+      </section>
+      <hr />
+      <section>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Business</a>
+          <a href="#">Entertainment</a>
+          <a href="#">Health</a>
+          <a href="#">Science</a>
+          <a href="#">Sports</a>
+          <a href="#">Technology</a>
+        </nav>
       </section>
     </div>
   );
