3dbc97a7055c1c733e69188cd858f44b3c6bcbb3
Mariana Téllez <tellezresendizmariana@gmail.com>
2024-02-05 13:13:34 -0600

    Updated background color

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Menu from "./Menu";
 
 function App() {
   // API https://newsapi.org/docs
-  return (<div className="App">
-    <Menu />
-  </div>);
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
 }
 
 export default App;
