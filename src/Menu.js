3970241186444095fb4285d8f370e10e9265d7ea
Mariana Téllez <tellezresendizmariana@gmail.com>
2024-02-12 13:37:26 -0600

    Added Navigationjs component

--- src/Menu.js
+++ src/Menu.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Menu.css";
-
+import Navigation from "./Navigation";
 import Footer from "./Footer";
 
 export default function Menu() {
   return (
     <div className="Menu">
-      
+      <Navigation />
       <Footer />
     </div>
   );
