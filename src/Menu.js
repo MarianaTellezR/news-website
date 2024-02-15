b570611dba13f49f5c602fd181c20beeb7346aa1
Mariana Téllez <tellezresendizmariana@gmail.com>
2024-02-15 10:21:15 -0600

    Added Home.js component

--- src/Menu.js
+++ src/Menu.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Menu.css";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
+import Home from "./Home";
 
 export default function Menu() {
   return (
     <div className="Menu">
       <Navigation />
+      <Home />
       <Footer />
     </div>
   );
